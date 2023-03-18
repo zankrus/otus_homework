8d36cdfcccd87326c3eb8dc1662452c5d2b72936
Robert Islyamov <rrislyamov@avito.ru>
2023-03-18 22:21:06 +0300

    dafuq 5 hwrk

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,80 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
-func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+// Run starts tasks in N goroutines and stops its work when receiving M errors from tasks.
+func Run(tasks []Task, n int, m int) error {
+	if m <= 0 {
+		return ErrErrorsLimitExceeded
+	}
+
+	wg := &sync.WaitGroup{}
+	errs := int64(0)
+
+	// Создаем каналы
+	taskCh := make(chan Task)
+	errCh := make(chan struct{})
+
+	// Добавляем в группу кол-во корутин
+	wg.Add(n)
+
+	// Пихаем горутины-обработчики
+	for i := 0; i < n; i++ {
+		go worker(m, wg, errCh, taskCh, &errs)
+	}
+
+	// Синхрониризуем каналы и пихаем таски в канал
+	for _, task := range tasks {
+		select {
+		case <-errCh:
+		case taskCh <- task:
+		}
+	}
+
+	// Тут не разобрался, без этого горутины не завершаются. Хотя из канала прочитаны все таски
+	close(taskCh)
+	wg.Wait()
+
+	select {
+	case <-errCh:
+		return ErrErrorsLimitExceeded
+	default:
+	}
+
 	return nil
+}
+
+func worker(
+	m int,
+	wg *sync.WaitGroup,
+	errCh chan struct{},
+	taskCh chan Task,
+	errs *int64,
+) {
+	defer wg.Done()
+
+	for {
+		select {
+		case <-errCh:
+			return
+
+		case task, ok := <-taskCh:
+			if !ok {
+				return
+			}
+
+			if err := task(); err != nil {
+				*errs++
+				if *errs == int64(m) {
+					errCh <- struct{}{}
+				}
+			}
+		}
+	}
 }
