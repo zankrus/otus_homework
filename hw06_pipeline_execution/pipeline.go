7a7e6f31f2ef7a98f7d7b6ce6c6831d0e7d19081
Robert Islyamov <rrislyamov@avito.ru>
2023-04-02 15:28:06 +0300

    ничего не понятно, но очень интересно

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,5 +1,7 @@
 package hw06pipelineexecution
 
+import "log"
+
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -9,6 +11,36 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := make(Bi)
+
+	if len(stages) == 0 {
+		close(out)
+		return out
+	}
+
+	go func() {
+		defer close(out)
+
+		stream := in
+
+		for _, stage := range stages {
+			stream = stage(stream)
+		}
+
+		for {
+			select {
+			case <-done:
+				return
+			case i, ok := <-stream:
+				log.Println(i)
+				if !ok {
+					return
+				}
+				out <- i
+			}
+		}
+
+	}()
+
+	return out
 }
