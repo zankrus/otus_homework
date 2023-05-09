16cb258e2cc5e174fda6af1b90327ed399f606a5
Robert Islyamov <rrislyamov@avito.ru>
2023-05-09 16:14:16 +0300

    main

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,60 @@
 package main
 
+import (
+	"errors"
+	"flag"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+var (
+	timeout               time.Duration
+	defaultTimeout        time.Duration = time.Second * 10
+	ErrWrongArgumentCount               = errors.New("wrong arguments count")
+	ErrCantConnect                      = errors.New("cannot connect to telnet")
+)
+
+func init() {
+	flag.DurationVar(&timeout, "timeout", defaultTimeout, "timeout duration")
+}
+
+func handleReceiver(errs chan<- error, client TelnetClient) {
+	errs <- client.Receive()
+}
+
+func handleSender(errs chan<- error, client TelnetClient) {
+	errs <- client.Send()
+}
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	flag.Parse()
+
+	length := len(os.Args)
+
+	if length != 3 && length != 4 {
+		log.Panic(ErrWrongArgumentCount)
+	}
+
+	host, port := os.Args[length-2], os.Args[length-1]
+
+	client := NewTelnetClient(net.JoinHostPort(host, port), timeout, os.Stdin, os.Stdout)
+	if err := client.Connect(); err != nil {
+		log.Panic(ErrCantConnect)
+	}
+	defer client.Close()
+
+	errsCh := make(chan error)
+	signCh := make(chan os.Signal, 1)
+	defer signal.Stop(signCh)
+
+	signal.Notify(signCh, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+
+	go handleReceiver(errsCh, client)
+	go handleSender(errsCh, client)
+
+	<-signCh
 }
